25b9cd0256a597046934c26aa8195d37bc047770
Joaco <joaquineliasvega@gmail.com>
2025-03-08 18:35:11 -0300

    inscripciones verificacion

--- back/controllers/inscription.controller.js
+++ back/controllers/inscription.controller.js
@@ -14,10 +14,10 @@ export const createABC = async (req, res)=>{
         const existingPerson = await ABC.findOne({document});
 
         if(existingPerson) {
-            return res.status(400).json({ message: "Ya estas registrado/a" });
+            return res.status(400).json({success: false, message: "Ya estas registrado/a" });
         }
         await newABC.save();
-        res.status(201).json({success: true, data: newABC});
+        res.status(201).json({success: true, data: newABC, message:"Inscripción creada con éxito"});
     } catch (error) {
         console.error("Error in Create ABC:", error.message);
         res.status(500).json({success: false, message: "Server Error"});
